7e31e00bdcc0079b060b586b8759ccaf605e0132
Saswati Choudhury <saswatichoudhury2227@gmail.com>
2026-05-13 12:44:50 +0530

    feat(server): integrate auth signup endpoint and database setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import path from 'path';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import { connectDB } from './lib/db.js';
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const app = express();
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json()); // req.body
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
@@ -23,8 +25,8 @@ if (process.env.NODE_ENV === "production") {
     });
 }
 
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 });
 
